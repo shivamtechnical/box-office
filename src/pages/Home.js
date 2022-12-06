f8ba616b34791b5973d3f5efceab5184ca86ffac
shivam technical <shivam8273753651@gmail.com>
2022-12-06 14:05:36 +0530

    display show result and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import MainPageLayout from "../components/mainpagelayout";
+import { apiGet } from "../misc/config";
 
 const Home = () => {
-  return <MainPageLayout>this is my home;</MainPageLayout>;
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResult(result);
+    });
+  };
+
+  const onInputChange = (ev) => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = (ev) => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const renderResults = () => {
+    if (result && result.length === 0) {
+      return <div>NO RESULT</div>;
+    }
+    if (result && result.length > 0) {
+      return (
+        <div>
+          {result.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        {" "}
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
 };
 export default Home;
